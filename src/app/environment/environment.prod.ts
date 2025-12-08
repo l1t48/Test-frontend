abaec877371861f55b68ddd3c6bb20399e9fcdff
l1t48 <adnanhmdan7@gmail.com>
2025-12-08 20:33:14 +0100

    environment.prod.ts fix

--- src/app/environment/environment.prod.ts
+++ src/app/environment/environment.prod.ts
@@ -1,7 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://test-backend-u24f.onrender.com//api',
-  // ADD YOUR SECOND VARIABLE HERE
-  baseUrlWithoutAPI: 'https://test-backend-u24f.onrender.com/' 
+  apiBaseUrl: 'https://test-backend-u24f.onrender.com/api',
+  baseUrlWithoutAPI: 'https://test-backend-u24f.onrender.com' 
 };
